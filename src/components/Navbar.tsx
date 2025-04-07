fb4b8e7df3057168e78799907f1f5338ea933809
Jari Saukkonen <jari.saukkonen@gmail.com>
2025-04-07 21:10:57 +0300

    Initial revision

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="h-8 bg-slate-800 text-white flex items-center justify-center">
+      <h1 className="text-lg font-medium">Komakallio Observatory</h1>
+    </nav>
+  );
+} 
